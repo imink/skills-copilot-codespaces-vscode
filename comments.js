e2bdc7577e06ab1afcb7ccd09182b69e9e682032
Patrick Wang <patrick.wang1029@gmail.com>
2023-08-03 08:37:29 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// create web server
+var express = require('express');
+var router = express.Router();
+var Comment = require('../models/comment');
+var User = require('../models/user');
+var Post = require('../models/post');
+
+// GET /comments
+// Get all comments
+router.get('/', function(req, res, next) {
+  Comment.find().populate('author').exec(function(err, comments) {
+    if (err) { return next(err); }
+    res.json(comments);
+  });
+});
+
+// POST /comments
+// Create a comment
+router.post('/', function(req, res, next) {
+  var comment = new Comment({
+    body: req.body.body });
+  });
+
+  
